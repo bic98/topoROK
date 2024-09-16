f465f33df795e74a0cad37ca64137638753de1fe
Inchan Baek <83804565+bic98@users.noreply.github.com>
2024-09-16 17:39:22 +0900

    topoCotour update requadMesh Length is 3.0

--- topoContour/topoContour/obj/Debug/net48/topoContour.AssemblyInfo.cs
+++ topoContour/topoContour/obj/Debug/net48/topoContour.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Description of topoContour")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+68078ae022eece1499194f7fedb40d3ef1fa48e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cdc2e57610792724d8aa22d5a42224eeacef02bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("topoContour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("topoContour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
